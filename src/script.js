ea56e5b8b8042f05592e394210a2fcc51ebd83cc
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-08-14 18:32:43 +0200

    [add] basic transformation for objects and group

--- src/script.js
+++ src/script.js
@@ -9,14 +9,43 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.position.y = -1
+group.rotation.x = 1
+group.scale.y = 0.5
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+)
+cube2.position.x = 1.5
+
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff})
+)
+cube3.position.x = -1.5
+
+group.add(cube3)
+
+//Axes Helper
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
+
 
 /**
  * Sizes
- */
+*/
 const sizes = {
     width: 800,
     height: 600
@@ -24,10 +53,16 @@ const sizes = {
 
 /**
  * Camera
- */
+*/
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+//camera.position.x = 1
+//camera.position.y = 1
 scene.add(camera)
+
+//lookAt method
+//camera.lookAt(mesh.position)
+//console.log(mesh.position.distanceTo(camera.position));
 
 /**
  * Renderer
